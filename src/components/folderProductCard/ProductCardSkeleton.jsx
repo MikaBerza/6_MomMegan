f423696b743a5745eba11421460a871fa6068012
Mika <mikberza@mail.ru>
2023-10-27 11:42:34 +0300

    Исправлен компонент (ProductCardSkeleton) -изменение вида карточек заглушек
    Fixed the component (ProductCardSkeleton)
    -change the type of placeholder cards

--- src/components/folderProductCard/ProductCardSkeleton.jsx
+++ src/components/folderProductCard/ProductCardSkeleton.jsx
@@ -6,17 +6,13 @@ const ProductCardSkeleton = () => (
   <ContentLoader
     className={style['product-card']}
     speed={1}
-    width={280}
-    height={457}
-    viewBox='0 0 280 457'
-    backgroundColor='var(--light-gray6)'
+    width={367}
+    height={433}
+    viewBox='0 0 367 433'
+    backgroundColor='#f3f3f3'
     foregroundColor='#ecebeb'
   >
-    <rect x='-1' y='290' rx='5' ry='5' width='280' height='26' />
-    <rect x='0' y='330' rx='5' ry='5' width='280' height='72' />
-    <rect x='0' y='415' rx='5' ry='5' width='90' height='28' />
-    <rect x='126' y='415' rx='5' ry='5' width='155' height='40' />
-    <rect x='10' y='13' rx='0' ry='0' width='260' height='260' />
+    <rect x='8' y='5' rx='0' ry='0' width='357' height='423' />
   </ContentLoader>
 );
 
